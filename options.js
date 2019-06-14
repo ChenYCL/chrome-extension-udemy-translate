bda4b22f279a6d5a97b7c0281e42dfc57d645b9a
ChenYCL <aa34913@126.com>
2019-06-14 22:39:46 +0800

    add Japanese support

--- options.js
+++ options.js
@@ -24,16 +24,23 @@ function restore(configInfoBefore) {
         $('input[name=key]').val(configInfoBefore.Key)
         $('input[name=apiType][value=' + configInfoBefore.apiType + ']').attr('checked', 'checked')
 
-        switch (configInfoBefore.apiType) {
-            case 'baidu':
-                $('li.baidu').find('input[name=baidu][value=' + configInfoBefore.aimLang + ']').attr('checked', 'checked');
-                break;
-            case 'yandex':
-                $('li.yandex').find('input[name=yandex][value=' + configInfoBefore.aimLang + ']').attr('checked', 'checked');
-                break;
-            default:
-                $('li.youdao').find('input[name=youdao][value=' + configInfoBefore.aimLang + ']').attr('checked', 'checked');
-        }
+        let select = $('#'+configInfoBefore.apiType).data('select');
+        select.val(configInfoBefore.aimLang)
+        // switch (configInfoBefore.apiType) {
+        //     case 'baidu':
+        //         // $('li.baidu').find('input[name=baidu][value=' + configInfoBefore.aimLang + ']').attr('checked', 'checked');
+        //         // $('#baidu').find('option[value='+configInfoBefore.aimLang+']')
+        //         let select = $('#baidu').data('select');
+        //         select.val(configInfoBefore.aimLang)
+        //         break;
+        //     case 'yandex':
+        //         // $('li.yandex').find('input[name=yandex][value=' + configInfoBefore.aimLang + ']').attr('checked', 'checked');
+        //         $('#yandex').find('option[value='+configInfoBefore.aimLang+']')
+        //         break;
+        //     default:
+        //         // $('li.youdao').find('input[name=youdao][value=' + configInfoBefore.aimLang + ']').attr('checked', 'checked');
+        //         $('#youdao').find('option[value='+configInfoBefore.aimLang+']')
+        // }
     })
 
 }
@@ -43,7 +50,8 @@ function config() {
     let config = {
         'apiType': $('input[name=apiType]:checked').val(),
         'apiKey': $('input[name=apiValue]').val(),
-        'aimLang': $('input[name=' + $('input[name=apiType]:checked').val() + ']:checked').val(),
+        // 'aimLang': $('select[id=' + $('input[name=apiType]:checked').val() + ']:checked').val(),
+        'aimLang': $('select[id='+$('input[name=apiType]:checked').val()+']').val(),
         'Key': $('input[name=key]').val(),
     }
     chrome.storage.sync.set({ udemy: JSON.stringify(config) }, function () {
@@ -54,6 +62,7 @@ function config() {
     return config;
 }
 
+// remove input id&apiID
 $('input[name=apiType]').on('click',function(){
     let apiType = null;
     let _this = this;
