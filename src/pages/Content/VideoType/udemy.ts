72709c830839521d3f7ac7ba1461a37eace1c604
ChenYCL <aa34913@126.com>
2020-04-21 20:26:37 +0800

    feat: udemy subtitle under video support

--- src/pages/Content/VideoType/udemy.ts
+++ src/pages/Content/VideoType/udemy.ts
@@ -1,6 +1,6 @@
 import { getItem } from '../modules/localStorage.js';
 // @ts-ignore
-import { hiddenSubtitleCssInject,dealSubtitle } from "../modules/utils.ts";
+import { hiddenSubtitleCssInject, dealSubtitle } from '../modules/utils.ts';
 
 // 1.get screen subtitle
 const sub = {
@@ -8,16 +8,35 @@ const sub = {
   current: '',
 };
 
-
-sub.pre = $('[data-purpose=captions-cue-text]').html();
+const getOriginText = () => {
+  let obj_text = '';
+  $('.well--container--2edq4').find('span').forEach((span) => {
+    obj_text += (span.innerText + ' ').replace('<br>', ' ')
+      .replace(/\[(.+)\]/, '');
+  });
+  return obj_text;
+};
+// if origin subtitle under video
+if ($('.well--container--2edq4').length) {
+  sub.pre = getOriginText();
+} else {
+  sub.pre = $('[data-purpose=captions-cue-text]').html();
+}
 
 const run = async () => {
   let plugin_status = await getItem('status');
   if (plugin_status) {
     // cover css
-    hiddenSubtitleCssInject(['div[class^="captions-display--vjs-ud-captions-cue-text"]',
-      '[data-purpose="captions-cue-text"]']);
-    let current = $('[data-purpose=captions-cue-text]').html();
+    let current = null;
+    if ($('.well--container--2edq4').length) {
+      current = getOriginText();
+      hiddenSubtitleCssInject(['.well--container--2edq4']);
+    } else {
+      current = $('[data-purpose=captions-cue-text]').html();
+      hiddenSubtitleCssInject(['div[class^="captions-display--vjs-ud-captions-cue-text"]',
+        '[data-purpose="captions-cue-text"]']);
+    }
+
 
     // when change send request ,then make same
 
@@ -44,7 +63,53 @@ run();
 chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
   console.log(JSON.stringify(request));
   if (sub.current !== sub.pre) {
-    dealSubtitle('[data-purpose=captions-cue-text]',request);
+    if ($('.well--container--2edq4').length) {
+      // @ts-ignore
+      chrome.storage.sync.get(null, (items) => {
+        const subtitle = `<div class="SUBTILTE"
+    style="
+    position: relative;
+    bottom:0px;
+    width:100%;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    white-space: pre-line;
+    writing-mode: horizontal-tb;
+    unicode-bidi: plaintext;
+    direction: ltr;
+    -webkit-box-decoration-break: clone;
+    box-decoration-break: clone;
+    background: ${items['backgroundColor']};
+    opacity: ${items['backgroundColor']};
+  ">
+     <div class="origin_subtitle"
+      style="
+          color:${items['origin_color']} !important;
+          font-weight:${items['origin_weight']} !important;
+          font-size:${items['origin_font']}px !important;;
+      "
+     >${request.origin}</div>
+      <div class="translate_subtitle"
+      style="
+          color: ${items['trans_color']} !important;
+          font-weight:${items['trans_weight']} !important;
+          font-size: ${items['trans_font']}px !important;
+      "
+      >${request.translate}</div>
+  </div>`;
+        let hasSubtitleDom = $('div.SUBTILTE').length === 0;
+        if (hasSubtitleDom) {
+          $('.well--container--2edq4').after(subtitle);
+        } else {
+          $('div.SUBTILTE').remove();
+          $('.well--container--2edq4').after(subtitle);
+        }
+      });
+
+    } else {
+      dealSubtitle('[data-purpose=captions-cue-text]', request);
+    }
   }
 });
 
