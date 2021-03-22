416ba866411fb687163c8d9fa9e7aa2049b52a8d
ChenYCL <aa34913@126.com>
2021-03-22 23:18:42 +0800

    feat: ui ad

--- src/containers/detailSetup/detailSetup.jsx
+++ src/containers/detailSetup/detailSetup.jsx
@@ -3,7 +3,11 @@ import { CompactPicker } from 'react-color';
 import Icon128 from '../../assets/img/icon-128.png';
 import './detailSetup.scss';
 import { Avatar, Switch, Slider, Divider, List } from 'antd';
-import { SettingFilled } from '@ant-design/icons';
+import {
+  SettingFilled,
+  RedEnvelopeOutlined,
+  CloseSquareOutlined,
+} from '@ant-design/icons';
 import { GithubFilled } from '@ant-design/icons';
 import { getItem, setItem } from '../../pages/Content/modules/localStorage';
 
@@ -59,6 +63,7 @@ const DetailComponent = () => {
     trans_weight: 700,
   });
 
+  const [promotionStatus, setPromotionStatus] = useState(true);
   //--------------------------------- read storage
   useEffect(() => {
     const obj = {};
@@ -104,6 +109,11 @@ const DetailComponent = () => {
     );
   };
 
+  // goto  promotion page
+  const promotionPage = () => {
+    setPromotionStatus(!promotionStatus);
+  };
+
   const wrapperSetColor = (func, params) => {
     const { color, event, key } = params;
     func(color, event, key);
@@ -132,160 +142,213 @@ const DetailComponent = () => {
           <div className={'brand'}>Udemy Translate</div>
         </div>
         <div className={'right'}>
-          <SettingFilled onClick={Setting} style={{ fontSize: '24px' }} />
+          {promotionStatus ? (
+            <RedEnvelopeOutlined
+              title={'特惠推广'}
+              onClick={promotionPage}
+              style={{ fontSize: '24px', marginRight: '10px', color: 'red' }}
+            />
+          ) : (
+            <CloseSquareOutlined
+              title={'关闭'}
+              onClick={promotionPage}
+              style={{ fontSize: '24px', marginRight: '10px', color: 'green' }}
+            />
+          )}
+          <SettingFilled
+            title={'设置'}
+            onClick={Setting}
+            style={{ fontSize: '24px' }}
+          />
         </div>
       </div>
       <Divider style={{ padding: 0, margin: 0 }} />
-      <div className={'content'}>
-        <Switch
-          checkedChildren="开"
-          unCheckedChildren="关"
-          checked={valueMap.status}
-          onChange={() => {
-            setItem('status', !valueMap.status).then(() => {
-              setValues({
-                ...valueMap,
-                status: !valueMap.status,
-              });
-            });
-          }}
-          style={{ fontSize: '4em' }}
-        />
-      </div>
-      <List>
-        <List.Item className={'flex'}>
-          <span style={{ display: 'inlineBlock' }}>[背景色]</span>
-          <CompactPicker
-            colors={colors}
-            color={valueMap.backgroundColor}
-            onChangeComplete={(color, event) => {
-              wrapperSetColor(setColor, {
-                color,
-                event,
-                key: 'backgroundColor',
-              });
-            }}
-            className={'CompactPicker'}
-          />
-        </List.Item>
-        <List.Item className={'flex'}>
-          <span>[背景透明]</span>
-          <Slider
-            style={{ width: '280px' }}
-            min={0}
-            max={1}
-            step={0.1}
-            onChange={(value) => {
-              setValues({
-                ...valueMap,
-                backgroundOpacity: value,
-              });
-              setItem('backgroundOpacity', value);
-            }}
-            value={valueMap.backgroundOpacity}
-          />
-        </List.Item>
-        <List.Item className={'flex'}>
-          <span>[原文大小]</span>
-          <Slider
-            style={{ width: '280px' }}
-            onChange={(value) => {
-              setValues({
-                ...valueMap,
-                origin_font: value,
-              });
-              setItem('origin_font', value);
-            }}
-            value={valueMap.origin_font}
-          />
-        </List.Item>
-        <List.Item className={'flex'}>
-          <span>[原文颜色]</span>
-          <CompactPicker
-            name="origin_color"
-            color={valueMap.origin_color}
-            onChangeComplete={(color, event) => {
-              wrapperSetColor(setColor, { color, event, key: 'origin_color' });
-            }}
-            className={'CompactPicker'}
-          />
-        </List.Item>
-        <List.Item className={'flex'}>
-          <span>[原文粗细]</span>
-          <Slider
-            style={{ width: '280px' }}
-            min={100}
-            max={700}
-            step={100}
-            onChange={(value) => {
-              setValues({
-                ...valueMap,
-                origin_weight: value,
-              });
-              setItem('origin_weight', value);
-            }}
-            value={valueMap.origin_weight}
-          />
-        </List.Item>
-        <List.Item className={'flex'}>
-          <span>[译文大小]</span>
-          <Slider
-            style={{ width: '280px' }}
-            onChange={(value) => {
-              setValues({
-                ...valueMap,
-                trans_font: value,
-              });
-              setItem('trans_font', value);
-            }}
-            value={valueMap.trans_font}
-          />
-        </List.Item>
-        <List.Item className={'flex'}>
-          <span>[译文颜色]</span>
-          <CompactPicker
-            name="trans_color"
-            color={valueMap.trans_color}
-            onChangeComplete={(color, event) => {
-              wrapperSetColor(setColor, { color, event, key: 'trans_color' });
-            }}
-            className={'CompactPicker'}
-          />
-        </List.Item>
-        <List.Item className={'flex'}>
-          <span>[译文粗细]</span>
-          <Slider
-            style={{ width: '280px' }}
-            min={100}
-            max={700}
-            step={100}
-            onChange={(value) => {
-              setValues({
-                ...valueMap,
-                trans_weight: value,
-              });
-              setItem('trans_weight', value);
-            }}
-            value={valueMap.trans_weight}
-          />
-        </List.Item>
-        <List.Item>
-          <a
-            href="https://github.com/ChenYCL/chrome-extension-udemy-translate"
-            target="__blank"
-          >
-            <GithubFilled
-              style={{
-                fontSize: '28px',
-                color: 'black',
-                marginLeft: '20px',
-                cursor: 'pointer',
+      {promotionStatus ? (
+        <div>
+          <div className={'content'}>
+            <Switch
+              checkedChildren="开"
+              unCheckedChildren="关"
+              checked={valueMap.status}
+              onChange={() => {
+                setItem('status', !valueMap.status).then(() => {
+                  setValues({
+                    ...valueMap,
+                    status: !valueMap.status,
+                  });
+                });
               }}
+              style={{ fontSize: '4em' }}
             />
-            有问题
-          </a>
-        </List.Item>
-      </List>
+          </div>
+          <List>
+            <List.Item className={'flex'}>
+              <span style={{ display: 'inlineBlock' }}>[背景色]</span>
+              <CompactPicker
+                colors={colors}
+                color={valueMap.backgroundColor}
+                onChangeComplete={(color, event) => {
+                  wrapperSetColor(setColor, {
+                    color,
+                    event,
+                    key: 'backgroundColor',
+                  });
+                }}
+                className={'CompactPicker'}
+              />
+            </List.Item>
+            <List.Item className={'flex'}>
+              <span>[背景透明]</span>
+              <Slider
+                style={{ width: '280px' }}
+                min={0}
+                max={1}
+                step={0.1}
+                onChange={(value) => {
+                  setValues({
+                    ...valueMap,
+                    backgroundOpacity: value,
+                  });
+                  setItem('backgroundOpacity', value);
+                }}
+                value={valueMap.backgroundOpacity}
+              />
+            </List.Item>
+            <List.Item className={'flex'}>
+              <span>[原文大小]</span>
+              <Slider
+                style={{ width: '280px' }}
+                onChange={(value) => {
+                  setValues({
+                    ...valueMap,
+                    origin_font: value,
+                  });
+                  setItem('origin_font', value);
+                }}
+                value={valueMap.origin_font}
+              />
+            </List.Item>
+            <List.Item className={'flex'}>
+              <span>[原文颜色]</span>
+              <CompactPicker
+                name="origin_color"
+                color={valueMap.origin_color}
+                onChangeComplete={(color, event) => {
+                  wrapperSetColor(setColor, {
+                    color,
+                    event,
+                    key: 'origin_color',
+                  });
+                }}
+                className={'CompactPicker'}
+              />
+            </List.Item>
+            <List.Item className={'flex'}>
+              <span>[原文粗细]</span>
+              <Slider
+                style={{ width: '280px' }}
+                min={100}
+                max={700}
+                step={100}
+                onChange={(value) => {
+                  setValues({
+                    ...valueMap,
+                    origin_weight: value,
+                  });
+                  setItem('origin_weight', value);
+                }}
+                value={valueMap.origin_weight}
+              />
+            </List.Item>
+            <List.Item className={'flex'}>
+              <span>[译文大小]</span>
+              <Slider
+                style={{ width: '280px' }}
+                onChange={(value) => {
+                  setValues({
+                    ...valueMap,
+                    trans_font: value,
+                  });
+                  setItem('trans_font', value);
+                }}
+                value={valueMap.trans_font}
+              />
+            </List.Item>
+            <List.Item className={'flex'}>
+              <span>[译文颜色]</span>
+              <CompactPicker
+                name="trans_color"
+                color={valueMap.trans_color}
+                onChangeComplete={(color, event) => {
+                  wrapperSetColor(setColor, {
+                    color,
+                    event,
+                    key: 'trans_color',
+                  });
+                }}
+                className={'CompactPicker'}
+              />
+            </List.Item>
+            <List.Item className={'flex'}>
+              <span>[译文粗细]</span>
+              <Slider
+                style={{ width: '280px' }}
+                min={100}
+                max={700}
+                step={100}
+                onChange={(value) => {
+                  setValues({
+                    ...valueMap,
+                    trans_weight: value,
+                  });
+                  setItem('trans_weight', value);
+                }}
+                value={valueMap.trans_weight}
+              />
+            </List.Item>
+            <List.Item>
+              <a
+                href="https://github.com/ChenYCL/chrome-extension-udemy-translate"
+                target="__blank"
+              >
+                <GithubFilled
+                  style={{
+                    fontSize: '28px',
+                    color: 'black',
+                    marginLeft: '20px',
+                    cursor: 'pointer',
+                  }}
+                />
+                有问题
+              </a>
+            </List.Item>
+          </List>
+        </div>
+      ) : (
+        <div
+          style={{
+            height: 'calc(100vh - 42px)',
+            background: 'radial-gradient(white, rgb(51, 65, 76))',
+          }}
+        >
+          <h1 style={{ textAlign: 'center', color: 'red' }}>专属优惠链接</h1>
+          <ul style={{ listStyle: 'none' }}>
+            <li>
+              <a
+                style={{
+                  color: 'white',
+                  textDecoration: 'underline',
+                  fontSize: '24px',
+                }}
+                href="https://www.naifei.shop/?sid=YqDFNq"
+                target="_blank"
+              >
+                1. 《奈飞小铺》新人专属特权优惠10元+5元(作者专属通道)
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
